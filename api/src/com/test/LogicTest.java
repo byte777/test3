74627f52b34ffed524a97754a201c8373af2a7a9
byte777 <234697380@qq.com>
2018-09-14 18:56:45 +0800

    第一次提交

---
+++ api/src/com/test/LogicTest.java
@@ -0,0 +1,16 @@
+package com.test;
+
+/**
+ * <pre>
+ *
+ * author：byte(234697380@qq.com)
+ * createTime：2018-09-14 18:53
+ * </pre>
+ */
+public class LogicTest{
+
+    public static void main(String[] args){
+        System.out.println("logic hello world");
+    }
+
+}
